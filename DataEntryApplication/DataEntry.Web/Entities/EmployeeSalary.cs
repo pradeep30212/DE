eb355553bfda396e3c18869b0960f4a9752619c3
Pradeep Pandit <pradeep30212@gmail.com>
2022-05-19 20:06:18 +0530

    Added asp.net mvc application

---
+++ DataEntryApplication/DataEntry.Web/Entities/EmployeeSalary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataEntry.Web.Entities
+{
+    public class EmployeeSalary
+    {
+        [Key]
+        public int EmployeeSalaryId { get; set; }
+        public int EmployeeId { get; set; }
+        public int MonthId { get; set; }
+        public int YearId { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ DataEntryApplication/DataEntry.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataEntry.Web.Startup))]
+namespace DataEntry.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
